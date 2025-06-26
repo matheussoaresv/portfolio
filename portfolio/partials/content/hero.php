9901a39211905b10fb035014ddfc96b009117d31
matheussoaresv <matheussoares198@gmail.com>
2025-06-26 13:40:53 -0300

    Ajustes

---
+++ portfolio/partials/content/hero.php
@@ -0,0 +1,9 @@
+ <section id="hero" class="container-lg pt-5">
+     <div class="row row-cols-1">
+         <div class="col">
+             <h1 class="text-center">Landing Pages, Websites & Branding</h1>
+         </div>
+     </div>
+
+     <div class="linha-fade" id="linhaFade"></div>
+ </section>
